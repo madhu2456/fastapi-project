ff39a3938e3a849f87d20bb03913f2519ecd7532
Madhu Dadi <madhu.kumar245@gmail.com>
2026-03-13 21:11:22 +0530

    working on deployment

--- app/services/model_service.py
+++ app/services/model_service.py
@@ -1,7 +1,7 @@
 import joblib
 import pandas as pd
 from app.core.config import settings
-from app.cache.redis_cache import set_cache_prediction, get_cache_prediction
+from app.cache.redis_cache import set_cached_prediction, get_cached_prediction
 
 
 model = joblib.load(settings.MODEL_PATH)
@@ -9,11 +9,11 @@ model = joblib.load(settings.MODEL_PATH)
 
 def predict_car_price(data: dict):
     cache_key = " ".join([str(val) for val in data.values()])
-    cached = get_cache_prediction(cache_key)
+    cached = get_cached_prediction(cache_key)
     if cached:
         return cached
 
     input_data = pd.DataFrame([data])
     prediction = model.predict(input_data)
-    set_cache_prediction(cache_key, prediction)
+    set_cached_prediction(cache_key, prediction)
     return prediction
